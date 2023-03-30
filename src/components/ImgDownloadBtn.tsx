5106812dfdefd5349d57522ce7e624772778a18e
yoo-jimin127 <dbwlals9936@naver.com>
2023-03-30 14:28:27 +0900

    comment: ImgDownloadBtn tsdoc 추가

--- src/components/ImgDownloadBtn.tsx
+++ src/components/ImgDownloadBtn.tsx
@@ -3,7 +3,10 @@ interface ImageProps {
     src: string,
     fileName: string,
 }
-
+/**
+ * @param src 다운로드받을 이미지의 경로 입력 (string)
+ * @param fileName src param을 로컬에 저장할 때 사용될 파일명 (string) ex) carrot.png
+ */
 const ImgDownloadBtn = ({src, fileName}: ImageProps) => {
     const handleDownload = () => {
         const link = document.createElement("a");
@@ -22,5 +25,3 @@ const ImgDownloadBtn = ({src, fileName}: ImageProps) => {
 };
 
 export default ImgDownloadBtn;
-
-// Usage : <ImgDownloadBtn src={testImg} fileName="carrot.png" />
