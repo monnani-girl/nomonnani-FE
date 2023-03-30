a5dd658b67bb9294a957ff3f7e85b3d70ff1f60e
yoo-jimin127 <dbwlals9936@naver.com>
2023-03-30 17:38:08 +0900

    fix: 버튼 Props 수정 & 스타일 세분화

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,37 +1,70 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import React, { ReactNode } from "react"
 
 interface ButtonProps {
-    text: ReactNode
-    style?: React.CSSProperties
+    children: ReactNode;
+    size: 'big' | 'medium' | 'small';
+    fill: boolean;
 }
 
-const Button = ({text, style}: ButtonProps) => {
+interface BtnContainerProps {
+    size: 'big' | 'medium' | 'small';
+    fill: boolean;
+}
+
+const Button = ({children, size, fill}: ButtonProps) => {
     return (
-        <BtnContainer style={style}>{text}</BtnContainer>
+        <BtnContainer size={size} fill={fill}>
+            {children}
+        </BtnContainer>
     );
 }
 
 export default Button;
 
-const BtnContainer = styled.button`
-    width: 159px;
-    height: 80px;
-    padding: 28px 56px;
-    background: #FFFFFF;
+const BtnContainer = styled.button<BtnContainerProps>`
     border: none;
     outline: none;
-    box-shadow: 0px 2px 7px rgba(0, 0, 0, 0.15);
-    border-radius: 40px;
-
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 18px;
-    line-height: 24px;
     color: #001358;
+    box-shadow: 0px 2px 7px rgba(0, 0, 0, 0.15);
 
+    ${({ size }) => {
+        switch (size) {
+            case "big":
+                return `
+                    width: 250px;
+                    height: 72px;
+                    font-size: 24px;
+                    border-radius: 100px;
+                    padding: 24px 114px;
+                `;
+            case "medium":
+                return `
+                    width: 332px;
+                    height: 64px;
+                    font-size: 18px;
+                    border-radius: 40px;
+                    padding: 20px 40px;
+                `;
+            case "small":
+                return `
+                    width: 159px;
+                    height: 80px;
+                    font-size: 14px;
+                    border-radius: 40px;
+                    padding: 28px 56px;
+                `;
+        }
+    }}
+    
+    ${props =>
+        props.fill &&
+        css`
+            background: #4AE7A4;
+            box-shadow: none;
+        `}
+    
     &:active {
-        border: 2px solid #4AE7A4;
+        border: 2px solid #4ae7a4;
     }
-`;
+    `;
