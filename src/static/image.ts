751d9a6c2ee63c2fbaf76f6a643baf9a7122d053
gayoung <gayoungyeom@gmail.com>
2023-03-31 10:48:46 +0900

    hotfix: 설명 추가

---
+++ src/static/image.ts
@@ -0,0 +1,15 @@
+import pumkinImg from '../assets/pumpkin.png';
+import broccoli from '../assets/broccoli.png';
+import potatoImg from '../assets/potato.png';
+import tangerineImg from '../assets/tangerine.png';
+import carrotImg from '../assets/carrot.png';
+import cabbageImg from '../assets/cabbage.png';
+
+export const image2Name = {
+  pumpkin: pumkinImg,
+  broccoli: broccoli,
+  potato: potatoImg,
+  tangerine: tangerineImg,
+  carrot: carrotImg,
+  cabbage: cabbageImg,
+};
