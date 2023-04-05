c8b260906253712cb9a62d06d0dc8e1f6ca59ec4
gayoung <gayoungyeom@gmail.com>
2023-04-05 15:17:13 +0900

    chore: api 예외처리 원복

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,12 +4,10 @@ import { SelectedProps } from './types';
 const baseURL = `http://k8s-monnani-aialb3-106f5dfdcf-870420322.ap-northeast-2.elb.amazonaws.com`;
 
 export const getResult = async (data: SelectedProps) => {
-  const res = await axios.post(`${baseURL}/result/product`, data);
-  return res.data;
-  // try {
-  //   const res = await axios.post(`${baseURL}/result/product`, data);
-  //   return res.data;
-  // } catch (e) {
-  //   console.log(e);
-  // }
+  try {
+    const res = await axios.post(`${baseURL}/result/product`, data);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
 };
