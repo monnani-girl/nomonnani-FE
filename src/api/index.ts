9fd7f7699a6612aa9a538a102d6bfc583fbe65a1
gayoung <gayoungyeom@gmail.com>
2023-04-08 20:12:09 +0900

    refact: react-query로 교체

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,10 +4,6 @@ import { SelectedProps } from './types';
 const baseURL = `https://api.ddokdarman.site`;
 
 export const getResult = async (data: SelectedProps) => {
-  try {
-    const res = await axios.post(`${baseURL}/crop/products`, data);
-    return res.data;
-  } catch (e) {
-    console.log(e);
-  }
+  const res = await axios.post(`${baseURL}/crop/products`, data);
+  return res.data;
 };
