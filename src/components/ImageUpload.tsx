abbc6ba3ba2ca2921e95e88093c9a16e88bde150
yoo-jimin127 <dbwlals9936@naver.com>
2023-04-23 20:26:47 +0900

    design: font 적용되지 않은 부분 수정

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -100,7 +100,7 @@ const FileSelctButton = styled.button`
 const FindButton = styled.button<{ disabled: boolean }>`
   width: 284px;
   height: 72px;
-  font-family: 'Gmarket Sans';
+  font-family: 'GmarketSansMedium';
   font-size: 24px;
   margin-top: 40px;
   color: var(--white);
