5187c18dd3df6c9f8cdda5b49439997a9345ad78
yoo-jimin127 <dbwlals9936@naver.com>
2023-04-23 16:00:13 +0900

    test: GA 테스트코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Router from './components/router';
 import { GlobalStyle } from './style/global';
 import './style/color.css';
+import RouteChangeTracker from './utils/RouteChangeTracker';
 
 function App() {
+  // RouteChangeTracker();
   return (
     <>
       <GlobalStyle />
