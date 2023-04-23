eee14cbe73a9b312c8f2810048fc5f5aef2e2669
yoo-jimin127 <dbwlals9936@naver.com>
2023-04-23 15:11:22 +0900

    fix: 이미지 업로드 로직 복구

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -129,10 +129,8 @@ function Result() {
                   border="2px solid var(--primary)"
                   onClick={() =>
                     handleImageDownload({
-                      title: `${QUOTE[resultType].name}`,
                       src: `${PRODUCT_IMAGES[resultType]}`,
                       fileName: 'ddokdarman.png',
-                      text: `${QUOTE[resultType].quote}`,
                     })
                   }
                 >
--- src/utils/ImageDownload.ts
+++ src/utils/ImageDownload.ts
@@ -1,46 +1,13 @@
 interface ImageProps {
-    src: string;
-    fileName: string;
-    title: string;
-    text: string;
+  src: string;
+  fileName: string;
 }
 
-export const handleImageDownload = ({ src, fileName, title, text }: ImageProps) => {
-    const canvas = document.createElement('canvas');
-    const img = new Image();
-
-    img.crossOrigin = 'anonymous';
-    img.src = src;
-
-    img.onload = function() {
-      canvas.width = img.width + 50;
-      canvas.height = img.height + 50;
-
-      // 반환 객체의 null 값 여부 확인
-      const ctx = canvas.getContext('2d');
-      if (!ctx) {
-        console.error('결과 이미지 저장을 실패했어요.');
-        return;
-      }
-      ctx.fillStyle = '#fffbf8'; 
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(img, 0, 0);
-
-      ctx.font = 'bold 16px GmarketSansMedium'; 
-      ctx.fillStyle = 'black';
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.fillText(title, canvas.width / 2, img.height + 25);
-
-      ctx.font = '14px Noto Sans KR'; 
-      ctx.fillStyle = 'gray';
-      ctx.fillText(text, canvas.width / 2, img.height + 45);
-      
-      const link = document.createElement('a');
-      link.download = fileName;
-      link.href = canvas.toDataURL('image/png').replace(/^data:image\/[^;]/, 'data:application/octet-stream');
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    };
-};
+export const handleImageDownload = ({src, fileName}: ImageProps) => {
+  const link = document.createElement('a');
+  link.href = src;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
