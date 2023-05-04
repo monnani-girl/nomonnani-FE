c758ece5851b69688e83509c09b7464f8c514e1a
yoo-jimin127 <dbwlals9936@naver.com>
2023-05-04 15:04:44 +0900

    chore: 헤더 조건부 렌더링 주석 해제

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  // TODO : 헤더 로고 이미지 조건부 렌더링 코드
-  // const logoSrc = location.pathname === '/result' ? resultHeaderLogo : headerLogo;
-  const logoSrc = headerLogo
+  const logoSrc = location.pathname === '/result' ? resultHeaderLogo : headerLogo;
 
   return <HeaderLogo src={logoSrc} width={logoSrc === resultHeaderLogo ? '92px' : '17px'} onClick={() => navigate('/')} />;
 };
