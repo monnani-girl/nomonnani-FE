8fd90a7b5f7b67611c4c68571db23d06ee37e864
yoo-jimin127 <dbwlals9936@naver.com>
2023-05-04 14:31:15 +0900

    comment: 헤더 로고 이미지 조건부 렌더링 코드 주석처리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,9 @@ import { useNavigate, useLocation } from 'react-router-dom';
 const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const logoSrc = location.pathname === '/result' ? resultHeaderLogo : headerLogo;
+  // TODO : 헤더 로고 이미지 조건부 렌더링 코드
+  // const logoSrc = location.pathname === '/result' ? resultHeaderLogo : headerLogo;
+  const logoSrc = headerLogo
 
   return <HeaderLogo src={logoSrc} width={logoSrc === resultHeaderLogo ? '92px' : '17px'} onClick={() => navigate('/')} />;
 };
